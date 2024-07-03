3d1f640f55bc998125b4b1d8367ee555563ebbd5
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-07-04 01:31:30 +0530

    Time: 145 ms (59.31%), Space: 40.8 MB (48.52%) - LeetHub

--- 0753-open-the-lock/0753-open-the-lock.cpp
+++ 0753-open-the-lock/0753-open-the-lock.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    int openLock(vector<string> &deadends, string target) {
+    int openLock(vector<string>& deadends, string target) {
         unordered_set<string> deadendsSet(deadends.begin(), deadends.end()), visited;
         int result = 0;
         if(deadendsSet.find("0000") != deadendsSet.end())
             return -1;
-        queue<pair<string, int>> wheelQueue;
-        wheelQueue.push({"0000", 0});
+        queue<pair<string, int>> q;
+        q.push({"0000", 0});
         visited.insert("0000");
-        while(!wheelQueue.empty())
+        while(!q.empty())
         {
-            string up, down, currentWheel = wheelQueue.front().first;
-            int result=wheelQueue.front().second;
-            wheelQueue.pop();
-            if (currentWheel == target)
+            string curr=q.front().first;
+            int result=q.front().second;
+            q.pop();
+            if(curr==target)
                 return result;
-            for (int i = 0; i < 4; i++)
+            for(int i=0;i<4;i++)
             {
-                down = up = currentWheel;
+                string down=curr, up=curr;
                 char upCh = up[i], downCh = down[i];
                 up[i] = (upCh == '9' ? '0' : upCh + 1);
                 down[i] = (downCh == '0' ? '9' : downCh - 1);
                 if (visited.find(up) == visited.end() && deadendsSet.find(up) == deadendsSet.end()) {
-                    wheelQueue.push({up, result+1});
+                    q.push({up, result+1});
                     visited.insert(up);
                 }
                 if (visited.find(down) == visited.end() && deadendsSet.find(down) == deadendsSet.end()) {
-                    wheelQueue.push({down, result+1});
+                    q.push({down, result+1});
                     visited.insert(down);
                 }
             }
