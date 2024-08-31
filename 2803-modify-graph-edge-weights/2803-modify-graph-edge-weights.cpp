40ecfb2a6d86dc9e0d16d575f731bb854f3a111f
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-08-31 10:09:37 +0530

    Time: 2599 ms (5.26%), Space: 766.8 MB (6.88%) - LeetHub

--- 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
+++ 2803-modify-graph-edge-weights/2803-modify-graph-edge-weights.cpp
@@ -1,33 +1,33 @@
 class Solution {
 public:
-    typedef long long ll;
-    const int LARGE_VALUE = 2e9;
-    typedef pair<long, long> P;
-    ll DijkstraAlgo(vector<vector<int>>& edges, int n, int src, int dest) {
-        unordered_map<ll, vector<pair<ll, ll>>> adj;
-        for(vector<int>& edge : edges) {
-            if(edge[2] != -1) {
-                int u  = edge[0];
-                int v  = edge[1];
-                int wt = edge[2];
+    long long f(vector<vector<int>>& edges, int n, int src, int dest)
+    {
+        unordered_map<long long, vector<pair<long long, long long>>> adj;
+        for(auto& edge:edges)
+        {
+            if(edge[2]!=-1)
+            {
+                int u = edge[0], v = edge[1], wt = edge[2];
                 adj[u].push_back({v, wt});
                 adj[v].push_back({u, wt});
             }
         }
-        priority_queue<P, vector<P>, greater<P>> pq;
-        vector<ll> result(n, INT_MAX);
-        vector<bool> visited(n, false);
+        priority_queue<pair<long, long>, vector<pair<long, long>>, greater<pair<long, long>>> pq;
+        vector<long long> result(n, INT_MAX);
+        vector<bool> visited(n);
         result[src] = 0;
         pq.push({0, src});
-        while(!pq.empty()) {
-            ll currDist = pq.top().first;
-            ll currNode = pq.top().second;
+        while(!pq.empty())
+        {
+            long long currDist = pq.top().first, currNode = pq.top().second;
             pq.pop();
             if(visited[currNode])
                 continue;
-            visited[currNode] = true;
-            for(auto&[nbr, wt] : adj[currNode]) {
-                if(result[nbr] > currDist + wt) {
+            visited[currNode] = 1;
+            for(auto& [nbr, wt]:adj[currNode])
+            {
+                if(result[nbr] > currDist + wt)
+                {
                     result[nbr] = currDist + wt;
                     pq.push({result[nbr], nbr});
                 }
@@ -36,24 +36,27 @@ public:
         return result[dest];
     }
     vector<vector<int>> modifiedGraphEdges(int n, vector<vector<int>>& edges, int source, int destination, int target) {
-        ll currShortestDist = DijkstraAlgo(edges, n, source, destination);
+        long long currShortestDist = f(edges, n, source, destination);
         if(currShortestDist < target)
             return {};
         bool matchedTarget = (currShortestDist == target);
-        for(vector<int>& edge : edges) {
-            if(edge[2] == -1) {
-                edge[2] = (matchedTarget == true) ? LARGE_VALUE : 1;
-                if(matchedTarget != true) {
-                    ll newShortestDist = DijkstraAlgo(edges, n, source, destination);
-
-                    if(newShortestDist <= target) {
-                        matchedTarget = true;
+        for(vector<int>& edge : edges)
+        {
+            if(edge[2] == -1)
+            {
+                edge[2] = (matchedTarget) ? 2e9 : 1;
+                if(!matchedTarget)
+                {
+                    long long newShortestDist = f(edges, n, source, destination);
+                    if(newShortestDist <= target)
+                    {
+                        matchedTarget = 1;
                         edge[2] += (target - newShortestDist);
                     }
                 }
             }
         }
-        if(matchedTarget == false)
+        if(!matchedTarget)
             return {};
         return edges;
     }
