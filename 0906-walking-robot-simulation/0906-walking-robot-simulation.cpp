2a2dc56c23d17cdfbd749035d88672e8d511e6ed
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-04 09:30:09 +0530

    Time: 134 ms (14.37%), Space: 41.7 MB (41.16%) - LeetHub

--- 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
+++ 0906-walking-robot-simulation/0906-walking-robot-simulation.cpp
@@ -1,11 +1,11 @@
 class Solution {
 public:
     int robotSim(vector<int>& commands, vector<vector<int>>& obstacles) {
-        vector<int> directions = {0, 1, 0, -1, 0};
+        vector<int> directions={0, 1, 0, -1, 0};
         unordered_set<string> st;
         for(auto i:obstacles)
             st.insert(to_string(i[0]) + "_" + to_string(i[1]));
-        int x = 0, y = 0, direction = 0, maxi = 0;
+        int x=0, y=0, direction=0, maxi=0;
         for(int i=0;i<commands.size();i++)
         {
             if(commands[i]==-2)
@@ -16,13 +16,12 @@ public:
             {
                 for(int j=0;j<commands[i];j++)
                 {
-                    int newX = x + directions[direction];
-                    int newY = y + directions[direction+1];
-                    if(st.find(to_string(newX) + "_" + to_string(newY)) == st.end())
+                    int newX = x+directions[direction];
+                    int newY = y+directions[direction+1];
+                    if(st.find(to_string(newX) + "_" + to_string(newY))==st.end())
                     {
-                        x = newX;
-                        y = newY;
-                        maxi = max(maxi, x*x + y*y);
+                        x=newX, y=newY;
+                        maxi=max(maxi, x*x + y*y);
                     }
                     else
                         break;
