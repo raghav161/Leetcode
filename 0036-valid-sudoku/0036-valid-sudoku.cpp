4a8d57355fc124e10ec564b9a19368a84baa62a8
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-04 23:22:45 +0530

    Time: 15 ms (72.83%), Space: 22 MB (87.85%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.cpp
+++ 0036-valid-sudoku/0036-valid-sudoku.cpp
@@ -1,23 +1,59 @@
 class Solution {
 public:
-    bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<string> st;
-        for(int i = 0; i<9; i++)
-        {
-            for(int j = 0; j<9; j++)
-            {
-                if(board[i][j] == '.')
-                    continue;
-                string row = string(1, board[i][j]) + "_row_" + to_string(i);
-                string col = string(1, board[i][j]) + "_col_" + to_string(j);
-                string box = string(1, board[i][j]) + "_box_" + to_string(i/3) + "_" + to_string(j/3);
-                if(st.count(row) || st.count(col) || st.count(box)) 
-                    return false;
-                st.insert(row);
-                st.insert(col);
-                st.insert(box);
+    bool isvalid(char ch, int r, int c, vector<vector<char>>& b) {
+        int row = b.size();
+        int col = b[0].size();
+
+        // checking if there is duplicate of ch in its row
+        for (int j = 0; j < col; j++) {
+
+            if (j!=c && b[r][j] == ch) {
+                return false;
             }
         }
+
+        // checking if there is duplicate of ch in its col
+        for (int i = 0; i < row; i++) {
+            if (i!=r && b[i][c] == ch) {
+                return false;
+            }
+        }
+
+        // checking is there is a duplicate in the box
+        int boxrow = (r/3)*3;
+        int boxcol = (c/3)*3;
+        int rowend = boxrow+3;
+        int colend = boxcol+3;
+        for (int i = boxrow; i < rowend; i++) {
+            for (int j = boxcol; j <colend; j++) {
+                if (i != r && j != c && b[i][j] == ch) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    bool isValidSudoku(vector<vector<char>>& board) {
+        int row = board.size();
+        int col = board[0].size();
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                char ch = board[i][j];
+                if (ch == '.') {
+                    continue;
+                }
+                
+                bool ans = isvalid(ch, i, j, board);
+
+                if (ans == false) {
+                    return false;
+                }
+            }
+        }
+
         return true;
     }
 };
