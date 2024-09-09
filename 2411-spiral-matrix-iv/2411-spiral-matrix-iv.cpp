322f6d4d98b15bee98cd0add87f44ce01212bfe2
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-09 09:44:09 +0530

    Time: 173 ms (31.09%), Space: 130.5 MB (65.45%) - LeetHub

--- 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/2411-spiral-matrix-iv.cpp
@@ -1,52 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> ans(m, vector<int>(n, -1));
-        int left=0, right=n-1, top=0, bottom=m-1;
-        ListNode* temp=head;
-        while(temp and top<=bottom and left<=right)
-        {
-            for(int i=left;i<=right and temp;i++)
-            {
-                ans[top][i] = temp->val;
-                temp = temp->next;
+        vector<vector<int>> matrix(m, vector<int>(n, -1));
+
+        int id    = 0; //0, 1, 2, 3
+        int top   = 0;
+        int down  = m-1;
+        int left  = 0;
+        int right = n-1;
+
+        while(top <= down && left <= right) {
+            if(id == 0) { //left to right
+                for(int i = left; head != NULL && i <= right; i++) {
+                    matrix[top][i] = head->val;
+                    head = head->next;
+                }
+                top++;
             }
-            top++;
-            if(top>bottom or temp==NULL)
-                break;
-            for(int i=top;i<=bottom and temp;i++)
-            {
-                ans[i][right] = temp->val;
-                temp = temp->next;
+
+            if(id == 1) { //top to down
+                for(int i = top; head != NULL && i <= down; i++) {
+                    matrix[i][right] = head->val;
+                    head = head->next;
+                }
+                right--;
             }
-            right--;
-            if(left>right or temp==NULL)
-                break;
-            for(int i=right;i>=left and temp;i--)
-            {
-                ans[bottom][i] = temp->val;
-                temp = temp->next;
+
+            if(id == 2) { //right to left
+                for(int i = right; head != NULL && i >= left; i--) {
+                    matrix[down][i] = head->val;
+                    head = head->next;
+                }
+                down--;
             }
-            bottom--;
-            if(top>bottom or temp==NULL)
-                break;
-            for(int i=bottom;i>=top and temp;i--)
-            {
-                ans[i][left] = temp->val;
-                temp = temp->next;
+
+            if(id == 3) { //down to top
+                for(int i = down; head != NULL && i >= top; i--) {
+                    matrix[i][left] = head->val;
+                    head = head->next;
+                }
+                left++;
             }
-            left++;
+
+            id = (id+1)%4; //0, 1, 2, 3
         }
-        return ans;
+
+        return matrix;
     }
 };
