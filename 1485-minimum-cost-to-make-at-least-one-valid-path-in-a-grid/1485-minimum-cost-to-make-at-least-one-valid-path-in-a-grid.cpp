3649f9111d0f26c56d1343cb868e6be1979dd093
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-14 13:53:25 +0530

    Time: 68 ms (35.92%), Space: 24.4 MB (32.39%) - LeetHub

---
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int dx[4] = {0, 0, 1, -1};
+    int dy[4] = {1, -1, 0, 0};
+    void bfs(int x, int y, vector<vector<int>>& dist, vector<vector<int>>& grid) {
+        queue<pair<int, int>> q;
+        q.push({x, y});
+        while(!q.empty()) {
+            int x1 = q.front().first, y1 = q.front().second;
+            q.pop();
+            for(int i=0;i<4;i++)
+            {
+                int wX = x1 + dx[i], wY = y1+ dy[i];
+                if (wX >= 0 && wX < grid.size() && wY >= 0 && wY < grid[0].size())
+                {
+                    if (i + 1 == grid[x1][y1] && dist[x1][y1] < dist[wX][wY]) {
+                        dist[wX][wY] = dist[x1][y1];
+                        q.push({wX, wY});
+                    }
+                    else if (dist[x1][y1] + 1 < dist[wX][wY]) {
+                        dist[wX][wY] = dist[x1][y1] + 1;
+                        q.push({wX, wY});
+                    }
+                }
+            }
+        }
+        return;
+    }
+    int minCost(vector<vector<int>>& grid) {
+        vector<vector<int>> dist(grid.size(), vector<int>(grid[0].size(), INT_MAX));
+        dist[0][0]=0;
+        bfs(0, 0, dist, grid);
+        return dist[grid.size()-1][grid[0].size()-1];
+    }
+};
