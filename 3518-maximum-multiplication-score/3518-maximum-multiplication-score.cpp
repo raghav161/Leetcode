a557f182cc008f50385799a9c80eb3a8dcbad652
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-15 08:20:54 +0530

    Time: 155 ms (33.33%), Space: 128.3 MB (100%) - LeetHub

---
+++ 3518-maximum-multiplication-score/3518-maximum-multiplication-score.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    long long maxScore(vector<int>& a, vector<int>& b) {
+        vector<long long> dp(5, LLONG_MIN);
+        dp[0] = 0;
+        for (int j = 0; j < b.size(); ++j) {
+            for (int i = 3; i >= 0; --i) {
+                if (dp[i] != LLONG_MIN)
+                    dp[i + 1] = max(dp[i + 1], dp[i] + (long long)a[i]*b[j]);
+            }
+        }
+        return dp[4];
+    }
+};
