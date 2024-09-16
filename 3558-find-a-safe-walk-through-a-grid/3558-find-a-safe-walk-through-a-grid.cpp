4afdc52d85017b5cbd0f0530c0a36c1e04b40ee0
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-16 22:37:16 +0530

    Time: 46 ms (60%), Space: 36.7 MB (60%) - LeetHub

--- 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.cpp
+++ 3558-find-a-safe-walk-through-a-grid/3558-find-a-safe-walk-through-a-grid.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
-    int directions[4][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
+    vector<int> dir = {1, 0, -1, 0, 1};
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
+        int m = grid.size(), n = grid[0].size();
         queue<pair<pair<int, int>, int>> q;
         vector<vector<int>> visited(m, vector<int>(n, -1));
         visited[0][0] = health - grid[0][0];
         q.push({{0, 0}, health - grid[0][0]});
-        
         while (!q.empty()) {
             auto current = q.front();
             q.pop();
-            
             int x = current.first.first;
             int y = current.first.second;
             int currentHealth = current.second;
             if (x == m - 1 && y == n - 1 && currentHealth >= 1)
                 return true;
             for (int i = 0; i < 4; i++) {
-                int newX = x + directions[i][0];
-                int newY = y + directions[i][1];
+                int newX = x + dir[i];
+                int newY = y + dir[i+1];
                 if (newX >= 0 && newX < m && newY >= 0 && newY < n) {
                     int newHealth = currentHealth - grid[newX][newY];
                     if (newHealth >= 1 and newHealth > visited[newX][newY]) {
