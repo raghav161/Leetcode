cdd0c4e068ce41e51bf672888bdcaa0a87bafe9c
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-16 20:15:24 +0530

    Time: 23 ms (6.62%), Space: 17.5 MB (88.24%) - LeetHub

--- 0539-minimum-time-difference/0539-minimum-time-difference.cpp
+++ 0539-minimum-time-difference/0539-minimum-time-difference.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     int findMinDifference(vector<string>& time) {
         sort(time.begin(), time.end());
-        int minDiff = INT_MAX;
-        int x = stoi(time[0].substr(0, 2));
-        string last=to_string(x+24) + ":" + time[0].substr(3, 5);
+        int mini = INT_MAX;
+        string last=to_string(stoi(time[0].substr(0, 2))+24) + ":" + time[0].substr(3, 5);
         time.push_back(last);
         for(int i=0;i<time.size()-1;i++)
         {
@@ -14,16 +13,10 @@ public:
             int b1 = stoi(time[i+1].substr(3, 5));
             int value = b1 + (60-a1) + (60*(b-a-1));
             if(b==a)
-                minDiff = min(minDiff, b1-a1);
+                mini = min(mini, b1-a1);
             else
-                minDiff = min(minDiff, value);
+                mini = min(mini, value);
         }
-        // int a = stoi(time[0].substr(0, 2));
-        // int b = stoi(time[time.size() - 1].substr(0, 2));
-        // int a1 = stoi(time[0].substr(3, 5));
-        // int b1 = stoi(time[time.size() - 1].substr(3, 5));
-        // int value = a1 + 60 - b1 + (60 * (24 + a - b - 1));
-        // return min(minDiff, value);
-        return minDiff;
+        return mini;
     }
 };
