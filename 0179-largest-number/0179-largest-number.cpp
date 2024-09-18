fb80284ffbbd636cf71625c730922258d5f9ed18
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-18 06:42:52 +0530

    Time: 4 ms (81.09%), Space: 17.5 MB (7.32%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    static bool comp(string a, string b){
+        return a+b>b+a;
+    }
+    string largestNumber(vector<int>& nums) {
+        vector<string> ans;
+        for(auto i:nums)
+            ans.push_back(to_string(i));
+        sort(ans.begin(), ans.end(), comp);
+        string res;
+        for(auto i:ans)
+            res+=i;
+        return res[0]=='0' ? "0":res;
+    }
+};
