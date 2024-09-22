d60fc7694440e7f06c336d3c2ca344e843c66633
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-22 08:34:07 +0530

    Time: 183 ms (77.78%), Space: 46.3 MB (66.67%) - LeetHub

--- 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.cpp
+++ 3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii/3572-count-substrings-that-can-be-rearranged-to-contain-a-string-ii.cpp
@@ -2,27 +2,27 @@ class Solution {
 public:
     long long validSubstringCount(string word1, string word2) {
         vector<int> mp1(26), mp2(26);
-        int missing = 0;
-        for (char c : word2)
-            mp1[c - 'a']++;
-        for (int i = 0; i < 26; i++) {
-            if (mp1[i] > 0)
-                missing += mp1[i];
+        int count=0;
+        for(auto c:word2)
+            mp1[c-'a']++;
+        for(int i=0;i<26;i++)
+        {
+            if(mp1[i]>0)
+                count+=mp1[i];
         }
-        long long ans = 0;
-        int left = 0;
-        int n = word1.length();
-        for (int right = 0; right < n; right++) {
-            int c = word1[right] - 'a';
-            mp2[c]++;
-            if (mp1[c] > 0 && mp2[c] <= mp1[c])
-                missing--;
-            while (missing == 0) {
-                ans += n - right;
-                int left_char = word1[left] - 'a';
-                if (mp1[left_char] > 0 && mp2[left_char] <= mp1[left_char])
-                    missing++;
-                mp2[left_char]--;
+        long long ans=0;
+        int left=0;
+        for(int right=0;right<word1.length();right++)
+        {
+            mp2[word1[right]-'a']++;
+            if(mp1[word1[right]-'a']>0 and mp2[word1[right]-'a']<=mp1[word1[right]-'a'])
+                count--;
+            while(count==0)
+            {
+                ans+=word1.length()-right;
+                if(mp1[word1[left]-'a']>0 and mp2[word1[left]-'a']<=mp1[word1[left]-'a'])
+                    count++;
+                mp2[word1[left]-'a']--;
                 left++;
             }
         }
