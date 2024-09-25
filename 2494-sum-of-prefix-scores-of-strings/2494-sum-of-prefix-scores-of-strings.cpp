fc63170bc63df533ef1fb685474b48eccea62e56
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-25 09:32:57 +0530

    Time: 657 ms (28.18%), Space: 704.2 MB (57.11%) - LeetHub

---
+++ 2494-sum-of-prefix-scores-of-strings/2494-sum-of-prefix-scores-of-strings.cpp
@@ -0,0 +1,62 @@
+struct TrieNode {
+    int countP = 0;
+    TrieNode* children[26];
+};
+
+class Solution {
+public:
+
+    TrieNode* getTrieNode() {
+        TrieNode* newNode = new TrieNode();
+        for(int i = 0; i < 26; i++) {
+            newNode->children[i] = nullptr;
+        }
+        newNode->countP = 0;
+        return newNode;
+    }
+
+    void insert(string& word, TrieNode* root) {
+        TrieNode* crawl = root;
+
+        for(char &ch : word) {
+            int idx = ch - 'a';
+
+            if(!crawl->children[idx]) {
+                crawl->children[idx] = getTrieNode();
+            }
+
+            crawl->children[idx]->countP += 1;
+            crawl = crawl->children[idx];
+        }
+    }
+
+    int getScore(string& word, TrieNode* root) {
+        TrieNode* crawl = root; //"abc"
+        int score = 0;
+
+        for(char &ch : word) {
+            int idx = ch - 'a';
+            score += crawl->children[idx]->countP;
+
+            crawl = crawl->children[idx]; // now i am at "b" wala node
+        }
+
+        return score;
+    }
+
+    vector<int> sumPrefixScores(vector<string>& words) {
+        int n = words.size();
+
+        TrieNode* root = getTrieNode();
+        for(string &word : words) {
+            insert(word, root);
+        }
+
+        vector<int> result(n);
+        for(int i = 0; i < n; i++) {
+            result[i] = getScore(words[i], root);
+        }
+
+        return result;
+    }
+};
