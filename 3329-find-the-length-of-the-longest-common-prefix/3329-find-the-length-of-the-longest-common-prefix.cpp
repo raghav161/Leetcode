405a5d54794b3dd522d41ad8e22e9a1685d239d1
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-25 09:31:13 +0530

    Time: 248 ms (73.78%), Space: 160.8 MB (54.76%) - LeetHub

--- 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/3329-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,32 +1,65 @@
+struct TrieNode {
+    //char digit;
+    TrieNode* children[10]; //0, 1, 2...9
+};
+
 class Solution {
 public:
+
+    TrieNode* getTrieNode() {
+        TrieNode* node = new TrieNode();
+        for(int i = 0; i < 10; i++) {
+            node->children[i] = nullptr;
+        }
+
+        return node;
+    }
+
+    void insert(int num, TrieNode* root) {
+        TrieNode* crawl = root;
+        string numStr = to_string(num);
+
+        for(char ch : numStr) {
+            int idx = ch - '0';
+            if(!crawl->children[idx]) {
+                crawl->children[idx] = getTrieNode();
+            }
+
+            crawl = crawl->children[idx];
+        }
+    }
+
+    //returns the length of th elongest prefix
+    int search(int num, TrieNode* root) {
+        TrieNode* crawl = root;
+        string numStr = to_string(num);
+        int length = 0;
+
+        for(char ch : numStr) {
+            int idx = ch - '0';
+            if(crawl->children[idx]) {
+                length++;
+                crawl = crawl->children[idx];
+            } else {
+                break;
+            }
+        }
+
+        return length;
+    }
+
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<int, int> mp1,mp2;
-        for(int i=0;i<arr1.size();i++)
-        {
-            int a = arr1[i];
-            while(a)
-            {
-                mp1[a]++;
-                a/=10;
-            }
+        TrieNode* root = getTrieNode(); //TRIE
+
+        for(int num : arr1) {
+            insert(num, root);
         }
-        for(int i=0;i<arr2.size();i++){
-            int a = arr2[i];
-            while(a){
-                mp2[a]++;
-                a/=10;
-            }
+
+        int result = 0;
+        for(int num : arr2) {
+            result = max(result, search(num, root));
         }
-        int ans=0;
-        for(auto i:mp1)
-        {
-            if(mp2.find(i.first)!=mp2.end())
-            {
-                string s=to_string(i.first);
-                ans=max(ans, (int)s.size());
-            }
-        }
-        return ans;
+
+        return result;
     }
 };
