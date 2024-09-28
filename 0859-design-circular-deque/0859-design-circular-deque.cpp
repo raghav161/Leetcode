fdfb9fe935d6b9fd048f1bee99834db8d6452e95
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-28 14:32:35 +0530

    Time: 30 ms (6.35%), Space: 22.8 MB (17.67%) - LeetHub

--- 0859-design-circular-deque/0859-design-circular-deque.cpp
+++ 0859-design-circular-deque/0859-design-circular-deque.cpp
@@ -1,80 +1,78 @@
 class MyCircularDeque {
+private:
+    list<int> deq;  // Using list instead of vector
+    int cnt;        // Current number of elements
+    int k;          // Maximum capacity
 public:
-    vector<int> deq;
-    int K;
-    int front;
-    int rear;
-    int currentCount;
-
+    /** Initialize your data structure here. Set the size of the deque to be k. */
     MyCircularDeque(int k) {
-        K = k;
-        deq.resize(k);
-        front = 0;
-        rear  = K-1;
-        currentCount = 0;
+        this->k = k;   // Assign capacity
+        cnt = 0;       // Initialize count to 0
     }
     
+    /** Adds an item at the front of Deque. Return true if the operation is successful. */
     bool insertFront(int value) {
-        if(isFull()) {
-            return false;
+        if (cnt == k) {
+            return false; // If the deque is full, insertion fails
         }
-
-        front = (front - 1 + K) % K;
-        deq[front] = value;
-        currentCount++;
+        deq.push_front(value); // Insert at the front of the deque
+        ++cnt;                 // Increment count
         return true;
     }
     
+    /** Adds an item at the rear of Deque. Return true if the operation is successful. */
     bool insertLast(int value) {
-        if(isFull()) {
-            return false;
+        if (cnt == k) {
+            return false; // If the deque is full, insertion fails
         }
-
-        rear = (rear + 1) % K;
-        deq[rear] = value;
-        currentCount++;
+        deq.push_back(value); // Insert at the rear of the deque
+        ++cnt;                // Increment count
         return true;
     }
     
+    /** Deletes an item from the front of Deque. Return true if the operation is successful. */
     bool deleteFront() {
-        if(isEmpty()) {
-            return false;
+        if (cnt == 0) {
+            return false; // If the deque is empty, deletion fails
         }
-        front = (front + 1) % K;
-        currentCount--;
+        deq.pop_front();  // Remove the front element
+        --cnt;            // Decrement count
         return true;
     }
     
+    /** Deletes an item from the rear of Deque. Return true if the operation is successful. */
     bool deleteLast() {
-        if(isEmpty()) {
-            return false;
+        if (cnt == 0) {
+            return false; // If the deque is empty, deletion fails
         }
-
-        rear = (rear - 1 + K) % K;
-        currentCount--;
+        deq.pop_back();   // Remove the last element
+        --cnt;            // Decrement count
         return true;
     }
     
+    /** Get the front item from the deque. */
     int getFront() {
-        if(isEmpty()) {
-            return -1;
-        }        
-
-        return deq[front];
-    }
-    
-    int getRear() {
-        if(isEmpty()) {
-            return -1;
+        if (cnt == 0) {
+            return -1; // If the deque is empty, return -1
         }
-        return deq[rear];
+        return deq.front(); // Return the front element
     }
     
+    /** Get the last item from the deque. */
+    int getRear() {
+        if (cnt == 0) {
+            return -1; // If the deque is empty, return -1
+        }
+        return deq.back(); // Return the last element
+    }
+    
+    /** Checks whether the circular deque is empty or not. */
     bool isEmpty() {
-        return currentCount == 0;
+        return cnt == 0; // Return true if the deque is empty
     }
     
+    /** Checks whether the circular deque is full or not. */
     bool isFull() {
-        return currentCount == K;
+        return cnt == k; // Return true if the deque is full
     }
 };
