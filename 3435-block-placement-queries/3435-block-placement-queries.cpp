c9cbfdad96c9560109b57a267c588a4701740ceb
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-09-30 23:07:26 +0530

    Time: 826 ms (91.73%), Space: 315.8 MB (89.47%) - LeetHub

--- 3435-block-placement-queries/3435-block-placement-queries.cpp
+++ 3435-block-placement-queries/3435-block-placement-queries.cpp
@@ -1,21 +1,23 @@
 class Solution {
 public:
     vector<bool> getResults(vector<vector<int>>& queries) {
-        map<int, int> space = {{0, 0}, {INT_MAX, INT_MAX}};
-        vector<bool> result;
+        map<int, int> mp = {{0, 0}, {INT_MAX, INT_MAX}};
+        vector<bool> ans;
         for (auto& q : queries) {
-            if (q[0] == 1) {
-                auto c = space.emplace(q[1], -1).first;
+            if (q[0] == 1)
+            {
+                auto c = mp.insert({q[1], -1}).first;
                 auto p = prev(c);
                 do {
                     c->second = max(c->first - p->first, p->second);
-                    p = c++;
-                } while (c != space.end() && c->second != p->second);
+                    p = c;
+                    c++;
+                } while (c != mp.end() && c->second != p->second);
             } else {
-                auto it = prev(space.upper_bound(q[1]));
-                result.push_back(it->second >= q[2] || q[1] - it->first >= q[2]);
+                auto it = prev(mp.upper_bound(q[1]));
+                ans.push_back(it->second >= q[2] || q[1] - it->first >= q[2]);
             }
         }
-        return result;
+        return ans;
     }
 };
