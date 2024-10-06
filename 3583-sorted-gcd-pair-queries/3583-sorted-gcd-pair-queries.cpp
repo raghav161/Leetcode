fa32c2bc01f44ed813ad484c4292d5db1e593418
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-06 09:14:48 +0530

    Time: 149 ms (100%), Space: 135.3 MB (25%) - LeetHub

---
+++ 3583-sorted-gcd-pair-queries/3583-sorted-gcd-pair-queries.cpp
@@ -0,0 +1,59 @@
+class Solution {
+public:
+    vector<int> gcdValues(vector<int>& nums, vector<long long>& queries) {
+        int n = nums.size();
+        int max_num = 0;
+        for (int num : nums) {
+            if (num > max_num) max_num = num;
+        }
+        vector<int> freq(max_num + 1, 0);
+        for (int num : nums) {
+            freq[num]++;
+        }
+        vector<long long> cnt(max_num + 1, 0);
+        for (int x = 1; x <= max_num; x++) {
+            for (int m = x; m <= max_num; m += x) {
+                cnt[x] += freq[m];
+            }
+        }
+        vector<long long> f(max_num + 1, 0);
+        for (int x = max_num; x >= 1; x--) {
+            f[x] = cnt[x] * (cnt[x] - 1) / 2;
+            for (int m = 2 * x; m <= max_num; m += x) {
+                f[x] -= f[m];
+            }
+        }
+        vector<int> gcd_values;
+        vector<long long> frequencies;
+        for (int x = 1; x <= max_num; x++) {
+            if (f[x] > 0) {
+                gcd_values.push_back(x);
+                frequencies.push_back(f[x]);
+            }
+        }
+        vector<long long> cum_freq(frequencies.size(), 0);
+        cum_freq[0] = frequencies[0];
+        for (size_t i = 1; i < frequencies.size(); i++) {
+            cum_freq[i] = cum_freq[i - 1] + frequencies[i];
+        }
+        long long total_pairs = (long long)n * (n - 1) / 2;
+        vector<int> ans;
+        for (long long q : queries) {
+            if (q >= total_pairs) {
+                ans.push_back(-1);
+                continue;
+            }
+            int left = 0, right = cum_freq.size() - 1;
+            while (left <= right) {
+                int mid = left + (right - left) / 2;
+                if (cum_freq[mid] > q) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            }
+            ans.push_back(gcd_values[left]);
+        }
+        return ans;
+    }
+};
