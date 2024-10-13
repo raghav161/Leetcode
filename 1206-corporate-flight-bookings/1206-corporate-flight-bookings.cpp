ca29890c41d94a49dbce30b001cae4bb6b6e859a
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-13 20:01:23 +0530

    Time: 163 ms (54.53%), Space: 78 MB (12.8%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-        vector<int> res(n + 1, 0);
-
-        for (auto it : bookings) {
-            res[it[0] - 1] += it[2];
-            res[it[1]] -= it[2];
+        vector<int> res(n);
+        for(auto it:bookings)
+        {
+            res[it[0]-1]+=it[2];
+            if(it[1]<n)
+                res[it[1]]-=it[2];
         }
-
-        for (int i = 1; i < n; i++)
-            res[i] += res[i - 1];
-
-        res.pop_back();
+        for(int i=1;i<n;i++)
+            res[i]+=res[i-1];
         return res;
     }
 };
