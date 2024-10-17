6739cc131872d417a27711ca414d7a92fe8f53ae
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-17 20:16:06 +0530

    Time: 437 ms (95.95%), Space: 566.8 MB (38.46%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.cpp
@@ -1,6 +1,5 @@
 class WordDictionary {
 public:
-    
     struct trieNode { 
         trieNode *children[26]; 
         bool isEndOfWord; 
@@ -8,61 +7,43 @@ public:
     
     trieNode* getNode() {
         trieNode* newNode = new trieNode();
-        newNode->isEndOfWord = false;
-        
-        for (int i = 0; i < 26; i++) {
+        newNode->isEndOfWord = false; 
+        for (int i = 0; i < 26; i++)
 			newNode->children[i] = NULL;
-		}
-        
         return newNode;
     }
-    
-    /** Initialize your data structure here. */
-    trieNode* root;
-    
+    trieNode* root;    
     WordDictionary() {
         root = getNode();
     }
-    
     void addWord(string word) {
-        trieNode *crawler = root; 
-        
+        trieNode *crawler = root;    
         for (int i = 0; i < word.length(); i++) { 
             int index = word[i] - 'a'; 
             if (!crawler->children[index]) 
                 crawler->children[index] = getNode();
-            
             crawler = crawler->children[index]; 
         }
-        
         crawler->isEndOfWord = true; 
     }
     
     bool searchUtil(trieNode* root, string word) {
         trieNode *crawler = root; 
-        
         for (int i = 0; i < word.length(); i++) { 
             char ch = word[i];
-            
             if(ch == '.') {
-                
-                for(int j = 0; j<26; j++) {
-                    if(crawler->children[j] != NULL)
-                        if(searchUtil(crawler->children[j], word.substr(i+1)))
-                            return true;
+                for(int j = 0; j<26; j++)
+                {
+                    if(crawler->children[j] and searchUtil(crawler->children[j], word.substr(i+1)))
+                        return true;
                 }
-                
                 return false;
-                
-            } else if (crawler->children[ch-'a'] == NULL) {
+            } else if (crawler->children[ch-'a'] == NULL)
                 return false;
-            }
-            
             crawler = crawler->children[ch-'a']; 
         }
-        return (crawler != NULL && crawler->isEndOfWord); 
+        return (crawler and crawler->isEndOfWord); 
     }
-    
     bool search(string word) {
         return searchUtil(root, word);
     }
