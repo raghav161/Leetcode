62e6e189adfe364effd4f4b4fd3dd2f4f587adc9
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-17 13:03:05 +0530

    Time: 110 ms (98.58%), Space: 66.4 MB (41.92%) - LeetHub

---
+++ 1512-design-underground-system/1512-design-underground-system.cpp
@@ -0,0 +1,21 @@
+class UndergroundSystem {
+public:
+    unordered_map<int, pair<string, int>> checkInMap;
+    unordered_map<string, pair<double, int>> routeMap;
+    UndergroundSystem() {}
+    void checkIn(int id, string stationName, int t) {
+        checkInMap[id] = {stationName, t};
+    }
+    void checkOut(int id, string stationName, int t) {
+        auto checkIn = checkInMap[id];
+        checkInMap.erase(id);
+        string routeName = checkIn.first + "_" + stationName;
+        routeMap[routeName].first += t - checkIn.second;
+        routeMap[routeName].second ++;
+    }
+    double getAverageTime(string startStation, string endStation) {
+        string routeName = startStation + "_" + endStation;
+        auto& route = routeMap[routeName];
+        return route.first / route.second;
+    }
+};
