d60bdeb0a6c8b337424b50b52395e0fdbf7e9ab1
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-25 14:10:38 +0530

    Time: 59 ms (92.86%), Space: 53.4 MB (64.56%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -4,9 +4,8 @@ public:
         sort(begin(folder), end(folder));
         vector<string> result;
         result.push_back(folder[0]);
-        for(int i = 1; i < folder.size(); i++) {
-            string currFolder = folder[i];
-            string lastFolder = result.back();
+        for(int i=1;i<folder.size();i++) {
+            string currFolder = folder[i], lastFolder = result.back();
             lastFolder += '/';
             if(currFolder.find(lastFolder))
                 result.push_back(currFolder);
