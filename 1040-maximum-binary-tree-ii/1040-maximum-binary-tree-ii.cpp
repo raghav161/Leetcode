5ba0192c7bc9a07e0a8d8707bf40ab7d7465c22a
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-29 00:12:30 +0530

    Time: 0 ms (100%), Space: 16.1 MB (53.31%) - LeetHub

--- 1040-maximum-binary-tree-ii/1040-maximum-binary-tree-ii.cpp
+++ 1040-maximum-binary-tree-ii/1040-maximum-binary-tree-ii.cpp
@@ -11,39 +11,13 @@
  */
 class Solution {
 public:
-    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
-        if(nums.empty())
-            return NULL;
-        auto i=max_element(nums.begin(), nums.end());
-        TreeNode* root = new TreeNode(*i);
-        if(i!=nums.begin())
-        {
-            vector<int> left(nums.begin(), i);
-            root->left=constructMaximumBinaryTree(left);
-        }
-        if(i!=nums.end()-1)
-        {
-            vector<int> right(i+1, nums.end());
-            root->right=constructMaximumBinaryTree(right);
-        }
-        return root;
-    }
-    vector<int> f(TreeNode* root)
-    {
-        if(root==NULL)
-            return {};
-        vector<int> left=f(root->left);
-        vector<int> right=f(root->right);
-        vector<int> ans(left.begin(), left.end());
-        ans.push_back(root->val);
-        ans.insert(ans.end(), right.begin(), right.end());
-        return ans;
-    }
     TreeNode* insertIntoMaxTree(TreeNode* root, int val) {
-        if(root==NULL)
-            return NULL;
-        vector<int> temp=f(root);
-        temp.push_back(val);
-        return constructMaximumBinaryTree(temp);
+        if (root == NULL or val > root->val) {
+            auto n = new TreeNode(val);
+            n->left = root;
+            return n;
+        }
+        root->right = insertIntoMaxTree(root->right, val);
+        return root;
     }
 };
