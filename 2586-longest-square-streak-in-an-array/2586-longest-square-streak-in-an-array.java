a24a9461ca3d1385cf58574255c08f712e48fb0b
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-10-28 11:13:18 +0530

    Time: 68 ms (12.5%), Space: 63.7 MB (5.56%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.java
@@ -3,16 +3,14 @@ class Solution {
         Arrays.sort(nums);
         Map<Integer, Integer> dp = new HashMap<>();
         int result = 0;
-
-        for (int num : nums) {
-            if (dp.containsKey((int) Math.sqrt(num)) && Math.sqrt(num) == (int) Math.sqrt(num)) {
+        for(int num : nums)
+        {
+            if(dp.containsKey((int) Math.sqrt(num)) && Math.sqrt(num) == (int) Math.sqrt(num))
                 dp.put(num, dp.get((int) Math.sqrt(num)) + 1);
-            } else {
+            else
                 dp.put(num, 1);
-            }
             result = Math.max(result, dp.get(num));
         }
-
         return result == 1 ? -1 : result;
     }
 }
