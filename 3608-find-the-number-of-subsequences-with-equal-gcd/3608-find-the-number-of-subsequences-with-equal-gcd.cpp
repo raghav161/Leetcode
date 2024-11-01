03f0a4cd898291323891fec071c81ffc05d0ecc9
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-01 23:26:32 +0530

    Time: 1094 ms (60.51%), Space: 57.2 MB (89.31%) - LeetHub

--- 3608-find-the-number-of-subsequences-with-equal-gcd/3608-find-the-number-of-subsequences-with-equal-gcd.cpp
+++ 3608-find-the-number-of-subsequences-with-equal-gcd/3608-find-the-number-of-subsequences-with-equal-gcd.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
-    const int mod = 1e9 + 7;
     int dp[200][201][201];
-    int solve(int i, vector<int> &nums, int first, int second){
-        if(i == nums.size())
-            return (first && second) && (first == second);
+    const int mod = 1e9 + 7;
+    int f(int i, vector<int>& nums, int first, int second){
+        if(i==nums.size())
+            return (first and second) and (first == second);
         if(dp[i][first][second] != -1)
             return dp[i][first][second];
-        int skip = solve(i + 1, nums, first, second);
-        int take1 = solve(i + 1, nums, __gcd(first, nums[i]), second);
-        int take2 = solve(i + 1, nums, first, __gcd(second, nums[i]));
-        return dp[i][first][second] = (0LL + skip + take1 + take2) % mod;
+        int skip = f(i+1, nums, first, second);
+        int takefirst = f(i+1, nums, __gcd(first, nums[i]), second);
+        int takesecond = f(i+1, nums, first, __gcd(second, nums[i]));
+        return dp[i][first][second] = (0LL + skip + takefirst + takesecond)%mod;
     }
     
     int subsequencePairCount(vector<int>& nums) {
         memset(dp, -1, sizeof(dp));
-        return solve(0, nums, 0, 0);
+        return f(0, nums, 0, 0);
     }
 };
