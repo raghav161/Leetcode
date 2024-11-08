d95e04e966b332972b85f1a58ece32143bd21b5f
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-08 22:44:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> result(n);
         stack<int> st;
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] >= arr[i])
+            while (!st.empty() && arr[st.top()] > arr[i])
                 st.pop();
             result[i] = st.empty() ? -1 : st.top();
             st.push(i);
@@ -17,7 +17,7 @@ public:
         vector<int> result(n);
         stack<int> st;
         for (int i = n - 1; i >= 0; i--) {
-            while (!st.empty() && arr[st.top()] >= arr[i])
+            while (!st.empty() && arr[st.top()] > arr[i])
                 st.pop();
             result[i] = st.empty() ? n : st.top();
             st.push(i);
