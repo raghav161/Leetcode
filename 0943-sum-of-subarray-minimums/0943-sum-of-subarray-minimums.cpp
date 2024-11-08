33621ff9548d06a745583891775aac447aab8375
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-08 22:44:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,24 +1,43 @@
+typedef unsigned long long ll;
 class Solution {
 public:
-    const int mod = 1e9 + 7;
-    int sumSubarrayMins(vector<int>& arr) {
-        int n = arr.size(), res = 0;
-        vector<int> sum(n);
+    vector<int> getNSL(vector<int>& arr, int n) {
+        vector<int> result(n);
         stack<int> st;
         for (int i = 0; i < n; i++) {
-            while (!st.empty() && arr[st.top()] > arr[i])
+            while (!st.empty() && arr[st.top()] >= arr[i])
                 st.pop();
-            int j = -1;
-            if (!st.empty())
-                j = st.top();
-            sum[i] = (i - j) * arr[i];
-            if (j >= 0)
-                sum[i] += sum[j];
-            sum[i] %= mod;
+            result[i] = st.empty() ? -1 : st.top();
             st.push(i);
         }
-        for (auto it : sum)
-            res = (res + it) % mod;
-        return res;
+        return result;
+    }
+    
+    vector<int> getNSR(vector<int>& arr, int n) {
+        vector<int> result(n);
+        stack<int> st;
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && arr[st.top()] >= arr[i])
+                st.pop();
+            result[i] = st.empty() ? n : st.top();
+            st.push(i);
+        }
+        return result;
+    }
+    
+    int sumSubarrayMins(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> NSL = getNSL(arr, n);
+        vector<int> NSR = getNSR(arr, n);
+        ll sum = 0;
+        int M = 1e9 + 7;
+        for (int i = 0; i < n; i++) {
+            ll d1 = i - NSL[i];
+            ll d2 = NSR[i] - i;
+            ll total_ways_for_i_min = d1 * d2;
+            ll sum_i_in_total_ways = arr[i] * total_ways_for_i_min;
+            sum = (sum + sum_i_in_total_ways) % M;
+        }
+        return sum;
     }
 };
