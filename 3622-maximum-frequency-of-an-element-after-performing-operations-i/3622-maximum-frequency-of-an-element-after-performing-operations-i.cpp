179783fbc5cdf5490fae7cfa786ee32a67080d01
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-09 22:16:28 +0530

    Time: 290 ms (72.22%), Space: 145 MB (61.11%) - LeetHub

---
+++ 3622-maximum-frequency-of-an-element-after-performing-operations-i/3622-maximum-frequency-of-an-element-after-performing-operations-i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxFrequency(vector<int>& nums, int k, int ops) {
+        sort(begin(nums), end(nums));
+        unordered_map<int, int> um;
+        for (int i : nums)
+            um[i]++;
+        int ans = 0;
+        for (int i = nums[0]; i <= nums.back(); i++) {
+            int a = lower_bound(begin(nums), end(nums), i - k) - begin(nums);
+            int b = upper_bound(begin(nums), end(nums), i + k) - begin(nums);
+            b--;
+            ans = max(ans, um[i] + min(b - a - um[i] + 1, ops));
+        }
+        return ans;
+    }
+};
