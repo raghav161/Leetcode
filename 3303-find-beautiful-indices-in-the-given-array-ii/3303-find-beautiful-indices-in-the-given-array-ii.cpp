ae4386130d0d3b79aca9689dc8b2a32e2b6dc4b7
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-10 18:52:23 +0530

    Time: 149 ms (21.39%), Space: 89.6 MB (63.55%) - LeetHub

--- 3303-find-beautiful-indices-in-the-given-array-ii/3303-find-beautiful-indices-in-the-given-array-ii.cpp
+++ 3303-find-beautiful-indices-in-the-given-array-ii/3303-find-beautiful-indices-in-the-given-array-ii.cpp
@@ -1,28 +1,8 @@
 class Solution {
 public:
-    vector<int> beautifulIndices(string s, string a, string b, int k) {
-        int n = s.size(), aLen = a.size(), bLen = b.size();
-        vector<int> lpsA(aLen), lpsB(bLen);
-        calculateLPSArray(a, lpsA, aLen);
-        calculateLPSArray(b, lpsB, bLen);
-        vector<int> aMatchesIdx, bMatchesIdx;
-        findMatchingIdx(s, a, lpsA, aMatchesIdx, n, aLen);
-        findMatchingIdx(s, b, lpsB, bMatchesIdx, n, bLen);
-        sort(bMatchesIdx.begin(), bMatchesIdx.end());
-        vector<int> ans;
-        for(int i = 0; i < aMatchesIdx.size(); ++i) {
-            int i_idx = aMatchesIdx[i];
-            auto lb = lower_bound(bMatchesIdx.begin(), bMatchesIdx.end(), i_idx - k);
-            auto ub = upper_bound(bMatchesIdx.begin(), bMatchesIdx.end(), i_idx + k);
-            if(lb != ub)
-                ans.push_back(i_idx);
-        }
-        sort(ans.begin(), ans.end());
-        return ans;
-    }
-    void calculateLPSArray(string& s, vector<int>& lps, int n) {
+    void lpss(string& s, vector<int>& lps) {
         int len = 0, i = 1;
-        while(i < n) {
+        while(i < s.size()) {
             if(s[i] == s[len]) {
                 len++;
                 lps[i] = len;
@@ -37,24 +17,44 @@ public:
         }
     }
 
-    void findMatchingIdx(string& s, string& t, vector<int>& lps, vector<int>& matchesIdx, int n, int len) {
-        int i = 0, idx = 0;
-        while(i < n) {
-            if(s[i] == t[idx]) {
+    void f(string& s, string& t, vector<int>& lps, vector<int>& matchesIdx, int len) {
+        int i = 0, j = 0;
+        while(i < s.size()) {
+            if(s[i] == t[j]) {
                 i++;
-                idx++;
+                j++;
             } 
-            if(idx == len) {
-                matchesIdx.push_back(i - idx);
-                idx = lps[idx - 1];
+            if(j == len) {
+                matchesIdx.push_back(i - j);
+                j = lps[j - 1];
             }
-            else if(idx<t.size() and s[i]!= t[idx])
+            else if(j<t.size() and s[i]!= t[j])
             {
-                if(idx)
-                    idx = lps[idx - 1];
+                if(j)
+                    j = lps[j - 1];
                 else
                     i++;
             }
         }
+    }
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        int n = s.size(), aLen = a.size(), bLen = b.size();
+        vector<int> lpsA(aLen), lpsB(bLen);
+        lpss(a, lpsA);
+        lpss(b, lpsB);
+        vector<int> aMatchesIdx, bMatchesIdx;
+        f(s, a, lpsA, aMatchesIdx, aLen);
+        f(s, b, lpsB, bMatchesIdx, bLen);
+        sort(bMatchesIdx.begin(), bMatchesIdx.end());
+        vector<int> ans;
+        for(int i = 0; i < aMatchesIdx.size(); ++i) {
+            int i_idx = aMatchesIdx[i];
+            auto lb = lower_bound(bMatchesIdx.begin(), bMatchesIdx.end(), i_idx - k);
+            auto ub = upper_bound(bMatchesIdx.begin(), bMatchesIdx.end(), i_idx + k);
+            if(lb != ub)
+                ans.push_back(i_idx);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
     }
 };
