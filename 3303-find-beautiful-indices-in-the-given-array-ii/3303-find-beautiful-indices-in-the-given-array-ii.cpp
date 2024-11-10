321b2604d0c6fa62b66180fce66387fc4df0e50b
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-10 18:49:04 +0530

    Time: 148 ms (22.46%), Space: 89.7 MB (63.22%) - LeetHub

---
+++ 3303-find-beautiful-indices-in-the-given-array-ii/3303-find-beautiful-indices-in-the-given-array-ii.cpp
@@ -0,0 +1,73 @@
+class Solution {
+public:
+    vector<int> beautifulIndices(string s, string a, string b, int k) {
+        int n = s.size();
+        int aLen = a.size();
+        int bLen = b.size();
+        vector<int> lpsA(aLen), lpsB(bLen);
+
+        calculateLPSArray(a, lpsA, aLen);
+        calculateLPSArray(b, lpsB, bLen);
+
+        vector<int> aMatchesIdx, bMatchesIdx;
+
+        findMatchingIdx(s, a, lpsA, aMatchesIdx, n, aLen);
+        findMatchingIdx(s, b, lpsB, bMatchesIdx, n, bLen);
+
+        sort(bMatchesIdx.begin(), bMatchesIdx.end());
+
+        vector<int> ans;
+        for(int i = 0; i < aMatchesIdx.size(); ++i) {
+            int i_idx = aMatchesIdx[i];
+            auto lb = lower_bound(bMatchesIdx.begin(), bMatchesIdx.end(), i_idx - k);
+            auto ub = upper_bound(bMatchesIdx.begin(), bMatchesIdx.end(), i_idx + k);
+            if(lb != ub) {
+                ans.push_back(i_idx);
+            }
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+    void calculateLPSArray(string& s, vector<int>& lps, int n) {
+        int len = 0;
+        lps[0] = 0;
+        int i = 1;
+        while(i < n) {
+            if(s[i] == s[len]) {
+                len++;
+                lps[i] = len;
+                i++;
+            }
+            else {
+                if(len != 0) {
+                    len = lps[len-1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+    }
+
+    void findMatchingIdx(string& s, string& t, vector<int>& lps, vector<int>& matchesIdx, int n, int len) {
+        int i = 0, idx = 0;
+        while(i < n) {
+            if(s[i] == t[idx]) {
+                i++;
+                idx++;
+            } 
+            if(idx == len) {
+                matchesIdx.push_back(i - idx);
+                idx = lps[idx - 1];
+            }
+            else if(idx<t.size() and s[i]!= t[idx])
+            {
+                if(idx)
+                    idx = lps[idx - 1];
+                else
+                    i++;
+            }
+        }
+    }
+
+};
