14564a1aa2644c88e18b626171a2ef93551bccc9
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-10 11:10:57 +0530

    Time: 2 ms (23.81%), Space: 26.8 MB (31.49%) - LeetHub

--- 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.cpp
+++ 3381-shortest-subarray-with-or-at-least-k-i/3381-shortest-subarray-with-or-at-least-k-i.cpp
@@ -1,17 +1,50 @@
 class Solution {
 public:
-    int minimumSubarrayLength(vector<int>& nums, int k) {
-        int n=nums.size(), mini=INT_MAX;
-        for(int i=0;i<n;i++)
-        {
-            int ans=0;
-            for(int j=i;j<n;j++)
-            {
-                ans=ans|nums[j];
-                if(ans>=k)
-                    mini=min(mini, j-i+1);
+    void fillBits(vector<int>& bits, int x) {
+        for (int i = 31; i >= 0; i--) {
+            if ((x & (1 << i)) != 0) {
+                bits[i]++;
             }
         }
-        return (mini==INT_MAX)?-1:mini;
+    }
+    int solve(vector<int>& bits) {
+        int ans = 0;
+        for (int i = 31; i >= 0; i--) {
+            if (bits[i] > 0) {
+                ans |= (1 << i);
+            }
+        }
+        return ans;
+    }
+    void updateBits(vector<int>& bits, int x) {
+        for (int i = 31; i >= 0; i--) {
+            if ((x & (1 << i)) != 0) {
+                bits[i]--;
+            }
+        }
+    }
+    int minimumSubarrayLength(vector<int>& nums, int k) {
+        int n = nums.size(), ans = INT_MAX;
+        for (auto i : nums) {
+            if (i >= k)
+                return 1;
+        }
+        int i = 0, j = 0;
+        int curor = 0;
+        vector<int> bits(32, 0);
+        while (j < n) {
+            curor |= nums[j];
+            fillBits(bits, nums[j]);
+            while (i <= j && curor >= k) {
+                ans = min(ans, j - i + 1);
+                updateBits(bits, nums[i]);
+                curor = solve(bits);
+                i++;
+            }
+            j++;
+        }
+        if (ans == INT_MAX)
+            return -1;
+        return ans;
     }
 };
