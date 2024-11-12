4765153b4608e2ea3d184e95d9696e85a9930569
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-12 23:04:47 +0530

    Time: 91 ms (51.72%), Space: 61.8 MB (63.42%) - LeetHub

--- 2526-longest-increasing-subsequence-ii/2526-longest-increasing-subsequence-ii.cpp
+++ 2526-longest-increasing-subsequence-ii/2526-longest-increasing-subsequence-ii.cpp
@@ -7,22 +7,23 @@ public:
         tree = vector<int>(4*n_);
     }
 
-    int max_value() { return tree[0]; }
-
-    int query(int l, int r) { return query_util(0, l, r, 0, n - 1); }
+    int max_value() { 
+        return tree[0]; 
+    }
 
     int query_util(int i, int qL, int qR, int l, int r) {
         if (l >= qL && r <= qR)
             return tree[i];
         if (l > qR || r < qL)
             return INT_MIN;
-
         int m = (l + r) / 2;
-        return max(query_util(2 * i + 1, qL, qR, l, m),
-                   query_util(2 * i + 2, qL, qR, m + 1, r));
+        return max(query_util(2*i+1, qL, qR, l, m), query_util(2*i+2, qL, qR, m+1, r));
+    }
+    
+    int query(int l, int r) { 
+        return query_util(0, l, r, 0, n - 1); 
     }
 
-    void update(int i, int val) { update_util(0, 0, n - 1, i, val); }
     void update_util(int i, int l, int r, int pos, int val) {
         if (pos < l || pos > r)
             return;
@@ -36,12 +37,15 @@ public:
         update_util(2 * i + 2, m + 1, r, pos, val);
         tree[i] = max(tree[2 * i + 1], tree[2 * i + 2]);
     }
+    void update(int i, int val){ 
+        update_util(0, 0, n - 1, i, val); 
+    }
 };
 
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums, int k) {
-        MaxSegmentTree tree(1e5 + 1);
+        MaxSegmentTree tree(*max_element(nums.begin(), nums.end())+1);
         for (int i : nums) {
             int lower = max(0, i - k);
             int cur = 1 + tree.query(lower, i - 1);
