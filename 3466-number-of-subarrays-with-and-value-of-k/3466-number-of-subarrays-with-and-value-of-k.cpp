6a6d1a125c69da202f5bd491ef9c93bb61983d87
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-13 01:46:00 +0530

    Time: 855 ms (28.73%), Space: 356.3 MB (15.09%) - LeetHub

---
+++ 3466-number-of-subarrays-with-and-value-of-k/3466-number-of-subarrays-with-and-value-of-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long countSubarrays(vector<int>& nums, int k) {
+        using ll = long long;
+        unordered_map<int, int> mp;
+        ll res = 0;
+        for(auto& num : nums)
+        {
+            unordered_map<int, int> temp;
+            temp[num]++;
+            for(auto& it : mp)
+            {
+                temp[it.first & num] += it.second;
+            }
+            res += temp[k];
+            mp = temp;
+        }
+        return res;
+        
+    }
+};
