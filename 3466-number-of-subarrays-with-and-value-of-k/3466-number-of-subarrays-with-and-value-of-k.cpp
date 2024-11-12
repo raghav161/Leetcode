10c45579593d6bddd67e00093b6e61fc30e4c0d0
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-13 01:53:29 +0530

    Time: 824 ms (32.98%), Space: 356.3 MB (15.09%) - LeetHub

--- 3466-number-of-subarrays-with-and-value-of-k/3466-number-of-subarrays-with-and-value-of-k.cpp
+++ 3466-number-of-subarrays-with-and-value-of-k/3466-number-of-subarrays-with-and-value-of-k.cpp
@@ -4,18 +4,15 @@ public:
         using ll = long long;
         unordered_map<int, int> mp;
         ll res = 0;
-        for(auto& num : nums)
+        for(auto& num:nums)
         {
             unordered_map<int, int> temp;
             temp[num]++;
-            for(auto& it : mp)
-            {
+            for(auto& it:mp)
                 temp[it.first & num] += it.second;
-            }
             res += temp[k];
             mp = temp;
         }
         return res;
-        
     }
 };
