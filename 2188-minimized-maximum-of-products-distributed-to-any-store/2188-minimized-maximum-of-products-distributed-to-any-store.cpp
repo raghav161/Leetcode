8bce8252809a7d8f2afff9f7d7372e59e714032c
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-14 09:09:09 +0530

    Time: 25 ms (89.14%), Space: 87.3 MB (34.13%) - LeetHub

---
+++ 2188-minimized-maximum-of-products-distributed-to-any-store/2188-minimized-maximum-of-products-distributed-to-any-store.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool possible(vector<int>& nums, int n, int mid)
+    {
+        int count=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]<=mid)
+                count++;
+            else
+                count+=ceil((double)nums[i]/mid);
+        }
+        return count<=n;
+    }
+    int minimizedMaximum(int n, vector<int>& nums) {
+        int left = 1, right = *max_element(nums.begin(), nums.end()), ans=INT_MAX;
+        while(left<=right) 
+        {
+            int mid = left + (right - left) / 2;
+            if(possible(nums, n, mid))
+            {
+                ans = min(ans, mid);
+                right = mid - 1;
+            }
+            else 
+                left = mid + 1;
+        }  
+        return ans;
+    }
+};
