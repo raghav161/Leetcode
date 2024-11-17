11153cf4809b272d6cdbdab196130c7ddce9af4f
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-17 08:49:59 +0530

    Time: 93 ms (24.34%), Space: 119.8 MB (18.41%) - LeetHub

---
+++ 0892-shortest-subarray-with-sum-at-least-k/0892-shortest-subarray-with-sum-at-least-k.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int shortestSubarray(vector<int> &nums, int k){
+        priority_queue<pair<long long, long long>, vector<pair<long long, long long>>, greater<pair<long long, long long>>> pq;
+        long long sum=0, ans=INT_MAX;
+        for(long long i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum>=k)
+                ans=min(ans, i+1);
+            while(!pq.empty() and (sum-pq.top().first)>=k)
+            {
+                ans=min(ans,i-pq.top().second);
+                pq.pop();
+            }
+            pq.push({sum, i});
+        }
+        return ans==INT_MAX?-1:ans;
+    }
+};
