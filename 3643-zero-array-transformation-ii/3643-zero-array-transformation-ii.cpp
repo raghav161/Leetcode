3d11a50bb72c968f349c864232b59ed5d9688cbe
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-11-17 10:25:49 +0530

    Time: 51 ms (90.91%), Space: 318.7 MB (66.67%) - LeetHub

---
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isZeroArray(vector<int>& nums, vector<vector<int>>& q, int k) {
+    vector<int> line(nums.size() + 1);
+    for (int i = 0; i < k; ++i) {
+        line[q[i][0]] += q[i][2];
+        line[q[i][1] + 1] -= q[i][2];
+    }
+    int cur = 0;
+    for (int i = 0; i < nums.size(); ++i) {
+        cur += line[i];
+        if (nums[i] > cur)
+            return false;
+    }
+    return true;
+}    
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        int l = 0, r = queries.size();
+        while (l <= r) {
+            int m = l + (r - l) / 2;
+            if (isZeroArray(nums, queries, m))
+                r = m - 1;
+            else
+                l = m + 1;
+        }
+        return l <= queries.size() ? l : -1;
+    }
+};
