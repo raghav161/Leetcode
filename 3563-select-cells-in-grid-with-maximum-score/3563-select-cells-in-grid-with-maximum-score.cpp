fc9c97ea471e314c175892b038f7c66ca10a5a1c
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-12-09 13:34:51 +0530

    Time: 2005 ms (5.35%), Space: 233.9 MB (21.24%) - LeetHub

--- 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.cpp
+++ 3563-select-cells-in-grid-with-maximum-score/3563-select-cells-in-grid-with-maximum-score.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
-    int f(vector<vector<int>>& values, int idx, int mask_row, map<pair<int, int>, int>& dp)
+    int f(vector<vector<int>>& values, int idx, int mask_row, map<string, int>& dp)
     {
-        if(idx == values.size())
+        if(idx==values.size())
             return 0;
-        if(dp.find({idx, mask_row})!= dp.end())
-            return dp[{idx, mask_row}];
+        string key=to_string(idx)+"_"+to_string(mask_row);
+        if(dp.count(key))
+            return dp[key];
         int nottake = f(values, idx+1, mask_row, dp), take=0;
         int row = values[idx][1];
         if(((1<<row) & mask_row)==0)
         {
             int j = idx;
-            while (j<values.size() and values[idx][0]==values[j][0])
+            while(j<values.size() and values[idx][0]==values[j][0])
                 j++;
             take = values[idx][0] + f(values, j, mask_row | (1<<row), dp);
         }
-        return dp[{idx, mask_row}] = max(nottake, take);
+        return dp[key] = max(nottake, take);
     }
     int maxScore(vector<vector<int>>& grid) {
         vector<vector<int>> values;
@@ -25,7 +26,7 @@ public:
                 values.push_back({grid[i][j], i, j});
         }
         sort(values.begin(), values.end());
-        map<pair<int, int>, int> dp;
+        map<string, int> dp;
         return f(values, 0, 0, dp);
     }
 };
