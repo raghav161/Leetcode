0f4314256dde4eb4b0ec07a967d6135df1d863cc
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-12-10 14:13:33 +0530

    Time: 1076 ms (10.94%), Space: 272.2 MB (27.73%) - LeetHub

--- 3266-find-longest-special-substring-that-occurs-thrice-ii/3266-find-longest-special-substring-that-occurs-thrice-ii.cpp
+++ 3266-find-longest-special-substring-that-occurs-thrice-ii/3266-find-longest-special-substring-that-occurs-thrice-ii.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        vector<vector<int>> matrix(26, vector<int>(s.length()+1));
+        unordered_map<char, map<int, int>> freqMap;
         char prev_char = s[0];
         int length = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s[i] == prev_char)
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == prev_char) {
                 length++;
-            else {
+            } else {
                 length = 1;
                 prev_char = s[i];
             }
-            matrix[s[i]-'a'][length]++;
+            freqMap[s[i]][length]++;
         }
         int ans = 0;
-        for(int i = 0; i < 26; i++) {
+        for (auto& charEntry : freqMap) {
             int cumSum = 0;
-            for(int j = s.length(); j >= 1; j--) {
-                cumSum += matrix[i][j];
-                if(cumSum >= 3) {
-                    ans = max(ans, j);
+            for (auto it = charEntry.second.rbegin(); it != charEntry.second.rend(); ++it) {
+                cumSum += it->second;
+                if (cumSum >= 3) {
+                    ans = max(ans, it->first);
                     break;
                 }
             }
