894e075e64ed7a9dee7ca97d14b504aca3c8ec58
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-12-10 14:21:02 +0530

    Time: 7 ms (83.54%), Space: 10.5 MB (87.05%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     int maximumLength(string s) {
-        unordered_map<string, int> mp;
-        for(int i=0;i<s.length();i++)
-        {
-            string temp = "";
-            temp += s[i];
-            mp[temp]++;
-            for(int j=i+1;j<s.length();j++)
-            {
-                if(s[i]==s[j])
-                {
-                    temp += s[j];
-                    mp[temp]++;
-                } 
-                else
+        unordered_map<char, vector<int>> mp;
+        char prev_char = s[0];
+        int length = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if (s[i] == prev_char)
+                length++;
+            else {
+                length = 1;
+                prev_char = s[i];
+            }
+            if(mp[s[i]].size()<=length)
+                mp[s[i]].resize(length+1, 0);
+            mp[s[i]][length]++;
+        }
+        int ans = 0;
+        for(auto& entry : mp) {
+            int cumSum = 0;
+            for (int len = entry.second.size() - 1; len >= 1; len--) {
+                cumSum += entry.second[len];
+                if (cumSum >= 3) {
+                    ans = max(ans, len);
                     break;
+                }
             }
         }
-        int ans=INT_MIN;
-        for(auto it:mp)
-        {
-            if(it.second>=3)
-                ans = max((int)it.first.size(), ans);
-        }
-        return ans == INT_MIN ? -1 : ans;
+        return ans == 0 ? -1 : ans;
     }
 };
