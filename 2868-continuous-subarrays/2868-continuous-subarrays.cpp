5981307d124d0b0b1451fc28b017f6c351488ae2
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-12-14 11:46:46 +0530

    Time: 220 ms (38.92%), Space: 155.9 MB (9.24%) - LeetHub

--- 2868-continuous-subarrays/2868-continuous-subarrays.cpp
+++ 2868-continuous-subarrays/2868-continuous-subarrays.cpp
@@ -1,19 +1,16 @@
 class Solution {
 public:
     long long continuousSubarrays(vector<int>& nums) {
-        int n = nums.size(), i=0, j=0;
-        multiset<int> st; 
-        long long ans=0;
-        while(i<n)
-        {
-            while(j<n && (st.size()==0 or max(nums[j], *st.rbegin())-min(*st.begin(), nums[j])<=2))
-            {
-                st.insert(nums[j]);
-                j++;
+        int rear = 0;
+        long long ans = 0;
+        multiset<int> st;
+        for (int front = 0; front < nums.size(); front++) {
+            st.insert(nums[front]);
+            while (*st.rbegin() - *st.begin() > 2) {
+                st.erase(st.find(nums[rear]));
+                rear++;
             }
-            ans+=st.size();
-            st.erase(st.find(nums[i]));
-            i++;
+            ans += front - rear + 1;
         }
         return ans;
     }
