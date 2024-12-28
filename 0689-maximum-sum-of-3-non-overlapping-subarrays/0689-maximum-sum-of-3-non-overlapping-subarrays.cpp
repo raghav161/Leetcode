aae2389c7ddb6720c16ecb6fe277562e02fa5df0
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-12-29 00:09:44 +0530

    Time: 155 ms (12.74%), Space: 58.6 MB (5.42%) - LeetHub

---
+++ 0689-maximum-sum-of-3-non-overlapping-subarrays/0689-maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int getMaxSum(vector<int>& pre, int i, int n, int k, vector<vector<int>>& memo, int c) {
+        if (c == 3 || i > n - k)
+            return 0;
+        if (memo[i][c])
+            return memo[i][c];
+        int sum = pre[i + k - 1] - (i > 0 ? pre[i - 1] : 0);
+        int keep = sum + getMaxSum(pre, i + k, n, k, memo, c + 1);
+        int skip = getMaxSum(pre, i + 1, n, k, memo, c);
+        return memo[i][c] = max(keep, skip);
+    }
+
+    vector<int> getIndexList(vector<int>& pre, int i, int n, int k, vector<int> indexList, vector<vector<int>>& memo, int c, int maxSum) {
+        if (c == 3 && maxSum==0)
+            return indexList;
+        if (c == 3 || i > n - k || memo[i][c] != maxSum) {
+            int m = 1e9;
+            return {m, m, m};
+        }
+        int sum = pre[i + k - 1] - (i > 0 ? pre[i - 1] : 0);
+        indexList.push_back(i);
+        auto keep = getIndexList(pre, i + k, n, k, indexList, memo, c + 1, maxSum - sum);
+        indexList.pop_back();
+        auto skip = getIndexList(pre, i + 1, n, k, indexList, memo, c, maxSum);
+        return min(keep, skip);
+    }
+
+    vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> pre(n + 1);
+        vector<vector<int>> memo(n + 1, vector<int>(3));
+        pre[0] = nums[0];
+        for (int i = 1; i < n; i++)
+            pre[i] = pre[i - 1] + nums[i];
+        int maxSum = getMaxSum(pre, 0, n, k, memo, 0);
+        return getIndexList(pre, 0, n, k, {}, memo, 0, maxSum);
+    }
+};
