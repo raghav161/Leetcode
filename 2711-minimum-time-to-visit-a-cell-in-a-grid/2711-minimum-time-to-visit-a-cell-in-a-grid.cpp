2f5d8a87faadec4f895cc56b8b3994f22909c3d0
raghav161 <107697186+raghav161@users.noreply.github.com>
2024-12-30 12:53:47 +0530

    Time: 225 ms (45.08%), Space: 63.1 MB (44.13%) - LeetHub

--- 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.cpp
+++ 2711-minimum-time-to-visit-a-cell-in-a-grid/2711-minimum-time-to-visit-a-cell-in-a-grid.cpp
@@ -4,11 +4,11 @@ public:
     int minimumTime(vector<vector<int>>& grid) {
         if (grid[0][1] > 1 && grid[1][0] > 1)
             return -1;
-        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size(), false));
+        vector<vector<bool>> visited(grid.size(), vector<bool>(grid[0].size()));
         priority_queue<pair<int, pair<int, int>>, vector<pair<int, pair<int, int>>>, greater<pair<int, pair<int, int>>>> pq;
         pq.push({grid[0][0], {0, 0}});
         while (!pq.empty()) {
-            int time = pq.top().first, row  = pq.top().second.first, col  = pq.top().second.second;
+            int time = pq.top().first, row = pq.top().second.first, col = pq.top().second.second;
             pq.pop();
             if (row == grid.size() - 1 && col == grid[0].size() - 1)
                 return time;
@@ -17,8 +17,7 @@ public:
             visited[row][col] = 1;
             for(int i=0;i<4;i++)
             {
-                int r = row + dir[i];
-                int c = col + dir[i+1];
+                int r = row + dir[i], c = col + dir[i+1];
                 if (r < 0 || r >= grid.size() || c < 0 || c >= grid[0].size() || visited[r][c])
                     continue;
                 if (grid[r][c] <= time+1)
