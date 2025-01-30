dfb7b4feaee51c4e3e96c5aeb612fe2f52428140
raghav161 <107697186+raghav161@users.noreply.github.com>
2025-01-30 23:01:35 +0530

    Time: 190 ms (28.99%), Space: 50.7 MB (69.33%) - LeetHub

--- 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/2583-divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    bool isBipartite(unordered_map<int, vector<int>> &adj, int curr, vector<int>& colors, int currColor) {
+    bool isBipartite(unordered_map<int, vector<int>>& adj, int curr, vector<int>& colors, int currColor) {
         colors[curr] = currColor;
         for(int &ngbr : adj[curr]) {
             if(colors[ngbr] == colors[curr])
-                return false;
+                return 0;
             if(colors[ngbr] == -1) {
-                if(isBipartite(adj, ngbr, colors, 1 - currColor) == false)
-                    return false;
+                if(!isBipartite(adj, ngbr, colors, 1 - currColor))
+                    return 0;
             }
         }
-        return true;
+        return 1;
     }
 
     int bfs(unordered_map<int, vector<int>> &adj, int currNode, int n) {
         queue<pair<int, int>> que;
-        vector<bool> visited(n, false);
+        vector<bool> visited(n);
         que.push({currNode, 1});
-        visited[currNode] = true;
+        visited[currNode] = 1;
         int ans=0;
         while(!que.empty()) {
             auto curr = que.front();
@@ -33,9 +33,9 @@ public:
         return ans;
     }
 
-    int getMaxFromEachComp(unordered_map<int, vector<int>> &adj, int curr, vector<bool>& visited, vector<int>& levels) {
+    int getMaxFromEachComp(unordered_map<int, vector<int>>& adj, int curr, vector<bool>& visited, vector<int>& levels) {
         int maxGrp = levels[curr];
-        visited[curr] = true;
+        visited[curr] = 1;
         for(int &ngbr : adj[curr]) {
             if(!visited[ngbr])
                 maxGrp = max(maxGrp, getMaxFromEachComp(adj, ngbr, visited, levels));
@@ -46,23 +46,22 @@ public:
     int magnificentSets(int n, vector<vector<int>>& edges) {
         unordered_map<int, vector<int>> adj;
         for(auto& edge : edges) {
-            int u = edge[0] - 1;
-            int v = edge[1] - 1;
+            int u = edge[0] - 1, v = edge[1] - 1;
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
         vector<int> colors(n, -1);
         for(int node = 0; node < n; node++) {
             if(colors[node] == -1) {
-                if(isBipartite(adj, node, colors, 1) == false)
+                if(!isBipartite(adj, node, colors, 1))
                     return -1;
             }
         }
-        vector<int> levels(n, 0);
+        vector<int> levels(n);
         for(int node = 0; node < n; node++)
             levels[node] = bfs(adj, node, n);
         int maxGroupEachComp = 0;
-        vector<bool> visited(n, false);
+        vector<bool> visited(n);
         for(int node = 0; node < n; node++) {
             if(!visited[node])
                 maxGroupEachComp += getMaxFromEachComp(adj, node, visited, levels);
