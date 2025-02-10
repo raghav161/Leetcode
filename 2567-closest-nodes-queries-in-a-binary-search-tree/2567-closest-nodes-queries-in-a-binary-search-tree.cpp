f78887c07757797cf751ff2cda3c29199a2d023c
raghav161 <107697186+raghav161@users.noreply.github.com>
2025-02-10 19:34:53 +0530

    Time: 76 ms (44.81%), Space: 196.8 MB (37.34%) - LeetHub

---
+++ 2567-closest-nodes-queries-in-a-binary-search-tree/2567-closest-nodes-queries-in-a-binary-search-tree.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& nums) {
+        if (root) {
+            inorder(root->left, nums);
+            nums.push_back(root->val);
+            inorder(root->right, nums);
+        }
+    }
+    vector<vector<int>> closestNodes(TreeNode* root, vector<int>& queries) {
+        vector<vector<int>> ans;
+        vector<int> nums;
+        inorder(root, nums);
+        for (auto q : queries) {
+            int mn = -1, mx = -1;
+            if (nums[0] <= q)
+                mn = (lower_bound(nums.begin(), nums.end(), q))-nums.begin();
+            if (q <= nums.back())
+                mx = (upper_bound(nums.begin(), nums.end(), q))-nums.begin();
+            if (mn > 0 && mn >= nums.size())
+                mn--;
+            if (mn >= 0 && nums[mn] > q)
+                mn--;
+            if (mx > 0 && nums[mx - 1] == q)
+                mx--;
+            if (mn != -1)
+                mn = nums[mn];
+            if (mx != -1)
+                mx = nums[mx];
+            ans.push_back({mn, mx});
+        }
+        return ans;
+    }
+};
